64aad68f19f4ca6a26ad1e2a47eb9b81c4003d72
Jagannathareddy P <87884540+Jagannath28@users.noreply.github.com>
2026-09-02 18:30:55 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
@@ -10,7 +9,6 @@ const Layout = ({ children }) => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className={`flex-grow ${bg}`} >{children}</main>
-      <Footer bg={bg} />
     </div>
   );
 };
